497322ab8ae2bdc9cdaae6a9adb0fc23afb6befb
Nail Valiullin <nvaliullin@timepath.ru>
2023-11-17 13:04:19 +0500

    init

--- www/VersionChecker.js
+++ www/VersionChecker.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
 exports.VersionCheck = function (urlGoogle, classGoogle, urlRuStore, classRuStore, success, error) {
-    exec(success, error, 'VersionChecker', 'VersionCheck', [urlGoogle], [classGoogle], [urlRuStore], [classRuStore]);
+    exec(success, error, 'VersionChecker', 'VersionCheck', [urlGoogle,classGoogle,urlRuStore,classRuStore]);
 };
